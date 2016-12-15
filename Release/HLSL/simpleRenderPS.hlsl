bbaf4e0c9cf4666a69973c6251358f3d22f6f404
Keisuke　Honda <nkc.keisuke.honda@gmail.com>
2016-12-15 23:24:17 +0900

    [update]

--- Release/HLSL/simpleRenderPS.hlsl
+++ Release/HLSL/simpleRenderPS.hlsl
@@ -1,23 +1,27 @@
-Texture2D txDiffuse : register( t0 );
-SamplerState samLinear : register( s0 );
+Texture2D txDiffuse : register(t0);
+SamplerState samLinear : register(s0);
 
-cbuffer cbMaterial : register( b0 )
+cbuffer cbMaterial : register(b3)
 {
-	float4 ambient;
-	float4 diffuse;
-	float3 specular;
-	float power;
-	float4 emmisive;
+	float4 g_Ambient;
+	float4 g_Diffuse;
+	float4 g_Specular;
+	float4 g_Emmisive;
+	float  g_specularPower;
+	float  g_transparency;
+	float  dammy;
+	float  dammy2;
 };
 
 struct PS_INPUT
 {
-    float4 Pos : SV_POSITION;
-	float2 Tex : TEXCOORD;
+	float4 Pos	: SV_POSITION;//位置
+	float3 Norm : NORMAL;//頂点法線
+	float2 Tex	: TEXCOORD;//テクスチャー座標
+	float4 Color : COLOR0;//最終カラー（頂点シェーダーにおいての）
 };
 
-float4 PS( PS_INPUT input) : SV_Target
+float4 PS(PS_INPUT input) : SV_Target
 {
-	float4 tex = txDiffuse.Sample( samLinear, input.Tex );
-	return diffuse;
+	return input.Color;
 }
--- Release/HLSL/simpleRenderVS.hlsl
+++ Release/HLSL/simpleRenderVS.hlsl
@@ -1,10 +1,3 @@
-//定義
-#define MAX_BONE_MATRICES 255
-
-//グローバル
-Texture2D g_texDecal: register(t0);//テクスチャーは レジスターt(n)
-SamplerState g_samLinear : register(s0);//サンプラーはレジスターs(n)
-
 cbuffer global_0:register(b0)
 {
 	float4 g_vLight;  //ライトの方向ベクトル
@@ -19,35 +12,24 @@ cbuffer global_1:register(b1)
 	matrix WVP;
 };
 
-cbuffer global_bones:register(b2)//ボーンのポーズ行列が入る
-{
-	matrix g_mConstBoneWorld[MAX_BONE_MATRICES];
-};
-
 cbuffer cbMaterial : register(b3)
 {
-	float4 g_Ambient = float4(0, 0, 0, 0);
-	float4 g_Diffuse = float4(1, 0, 0, 0);
-	float4 g_Specular = float4(1, 1, 1, 1);
-	float g_Power;
+	float4 g_Ambient;
+	float4 g_Diffuse;
+	float4 g_Specular;
 	float4 g_Emmisive;
+	float  g_specularPower;
+	float  g_transparency;
+	float  dammy;
+	float  dammy2;
 };
 
-
-//スキニング後の頂点・法線が入る
-struct Skin
-{
-	float4 Pos;
-	float3 Norm;
-};
 //バーテックスバッファーの入力
-struct VSSkinIn
+struct VS_INPUT
 {
 	float3 Pos	: POSITION;//位置   
 	float3 Norm : NORMAL;//頂点法線
 	float2 Tex	: TEXCOORD;//テクスチャー座標
-	uint4  Bones : BONE_INDEX;//ボーンのインデックス
-	float4 Weights : BONE_WEIGHT;//ボーンの重み
 };
 //ピクセルシェーダーの入力（バーテックスバッファーの出力）　
 struct PSSkinIn
@@ -61,16 +43,16 @@ struct PSSkinIn
 //
 //PSSkinIn VSSkin(VSSkinIn input )
 //バーテックスシェーダー
-PSSkinIn vs_main(VSSkinIn input)
+PSSkinIn vs_main(VS_INPUT input)
 {
 	PSSkinIn output;
 
 	output.Pos = mul(float4(input.Pos, 1.0f), WVP);
-	output.Norm = input.Norm;
+	output.Norm = mul(input.Norm, (float3x3)World);
 	output.Tex = input.Tex;
-	float3 LightDir = normalize(g_vLight);
-	float3 PosWorld = mul(input.Pos, World);
-	float3 ViewDir = normalize(g_vEye - PosWorld);
+	float3 LightDir = normalize((float3)g_vLight);
+	float3 PosWorld = mul((float3)output.Pos, World);
+	float3 ViewDir = normalize((float3)g_vEye - PosWorld);
 	float3 Normal = normalize(output.Norm);
 	float4 NL = saturate(dot(Normal, LightDir));
 
@@ -80,6 +62,7 @@ PSSkinIn vs_main(VSSkinIn input)
 
 
 	output.Color = g_Diffuse * NL + specular*g_Specular;
+	output.Color.w = g_transparency;
 
 	return output;
 }
